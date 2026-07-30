52b8915fb4912a06ffaa72104c25c85ec4487035
Muhammad Awais <kingyt6624@gmail.com>
2026-07-30 13:12:51 +0500

    feat: scaffold project base with HTML entry point and app constants

---
+++ src/config/constants.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = 'https://dummyjson.com/products';
+export const DEBOUNCE_DELAY = 350;
+export const CACHE_TTL_MS = 5 * 60 * 1000;
+export const DEFAULT_LIMIT = 12;
+export const STORAGE_KEYS = {
+  RECENT_SEARCHES: 'api_search_recent_v1',
+};
